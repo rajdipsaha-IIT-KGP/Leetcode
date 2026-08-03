7e133178b3ba4791d3222d330a9e42a202576974
Rajdip Saha <rajdipsaha7697@gmail.com>
2026-08-03 19:39:27 +0530

    Time: 0 ms (100%), Space: 12.8 MB (48.61%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,10 +12,8 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if((p && !q) || (!p && q))
-        return false;
-        if(!p && !q)
-        return true;
+        if(p == NULL || q == NULL)
+        return p == q;
         return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
