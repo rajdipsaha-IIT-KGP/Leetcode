50c0c59ad575694dbc2fb34244de8f3d1002ff87
Rajdip Saha <rajdipsaha7697@gmail.com>
2026-08-07 22:54:12 +0530

    Time: 8 ms (34.32%), Space: 19.3 MB (54.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,38 +2,36 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
-        vector<int>adj[n];
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
 
-        vector<int>indegree(n,0);
-
-        for(auto it : prerequisites){
-            adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
+        for(int i = 0 ; i < prerequisites.size() ; i++){
+           adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+           indegree[prerequisites[i][0]]++;
         }
-
-        queue<int>q;
-
-        for(int i = 0 ; i < indegree.size() ; i++){
-            if(indegree[i] == 0)
-            q.push(i);
-        }
-
-      vector<int>topoSort;
-
-      while(!q.empty()){
-        int node = q.front();
-        q.pop();
-
-        topoSort.push_back(node);
-        
-        for(auto it : adj[node]){
-            indegree[it]--;
-            if(indegree[it] == 0)
-            q.push(it);
-        }
+     
+     queue<int>q;
+     for(int i = 0 ; i < n ; i++){
+      if(indegree[i] == 0){
+         q.push(i);
       }
+     }
 
-      return topoSort.size() == n;
-        
+     vector<int>topo;
+
+     while(!q.empty()){
+      int node = q.front();
+      q.pop();
+      topo.push_back(node);
+      for(auto it : adj[node]){
+         indegree[it]--;
+         if(indegree[it] == 0){
+            q.push(it);
+         }
+      }
+     }
+
+     return topo.size() == numCourses;
+
     }
 };
