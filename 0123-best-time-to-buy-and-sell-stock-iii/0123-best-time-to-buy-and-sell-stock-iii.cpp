0c7ef339315f6efa227645a1e785c6c43f0dda33
Rajdip Saha <rajdipsaha7697@gmail.com>
2026-08-27 17:58:49 +0530

    Time: 599 ms (5.03%), Space: 291.2 MB (5.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,43 @@
 class Solution {
-public: 
-    int f(vector<int>& prices,vector<vector<vector<int>>>& dp,int index,int cap,int canBuyStock){
-       if(index == prices.size() || cap == 0){
-          return 0;
-       }
-       if(dp[index][cap][canBuyStock] != -1)
-       return dp[index][cap][canBuyStock];
-        int profit = 0;
+public:
+    int f(vector<vector<vector<int>>>& dp,
+          vector<int>& prices,
+          int index,
+          int cap,
+          int canBuy) {
 
-       if(canBuyStock == 1){
-          profit = max(-prices[index] + f(prices,dp,index+1,cap,0),f(prices,dp,index+1,cap,1));
-       } else {
-          profit = max(prices[index] + f(prices,dp,index+1,cap-1,1),f(prices,dp,index+1,cap,0));
-       }
-       return dp[index][cap][canBuyStock] = profit;
+        if(index == prices.size() || cap == 0)
+            return 0;
+
+        if(dp[index][cap][canBuy] != -1)
+            return dp[index][cap][canBuy];
+
+        int profit;
+
+        if(canBuy) {
+            profit = max(
+                -prices[index] + f(dp, prices, index + 1, cap, 0),
+                f(dp, prices, index + 1, cap, 1)
+            );
+        }
+        else {
+            profit = max(
+                prices[index] + f(dp, prices, index + 1, cap - 1, 1),
+                f(dp, prices, index + 1, cap, 0)
+            );
+        }
+
+        return dp[index][cap][canBuy] = profit;
     }
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(3,vector<int>(2,-1)));
-        return f(prices,dp,0,2,1);
+
+        vector<vector<vector<int>>> dp(
+            n,
+            vector<vector<int>>(3, vector<int>(2, -1))
+        );
+
+        return f(dp, prices, 0, 2, 1);
     }
 };
